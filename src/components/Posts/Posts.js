58d95ab94d34f460b4ded82213411106f5b504ae
bkchu <brandon.k.chung@gmail.com>
2018-04-07 23:58:05 -0500

    fixed a bug where going from one user profile to another didn't update the posts.

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -8,6 +8,21 @@ import Error from "../Error/Error";
 import "./Posts.css";
 
 class Posts extends Component {
+  // state = {
+
+  // }
+
+  componentDidUpdate(prevProps) {
+    if (
+      prevProps.match &&
+      this.props.match &&
+      prevProps.match.url !== this.props.match.url &&
+      this.props.match.path === "/users/:userid"
+    ) {
+      this.props.getPostsByUserId(this.props.match.params.userid);
+    }
+  }
+
   componentDidMount() {
     window.scrollTo(0, 0);
     if (this.props.match && this.props.match.path === "/users/:userid") {
@@ -16,35 +31,39 @@ class Posts extends Component {
       this.props.getPosts();
     }
   }
+
   render() {
-    let classes = ["Posts"];
-    if (!this.props.loading) {
-      classes.push("fade-in");
-    }
-    let { posts, error } = this.props;
-    let displayPosts = <p>Loading...</p>;
-    if (posts && !error) {
-      displayPosts = posts.map(post => {
-        return (
-          <Post
-            key={post.id}
-            id={post.id}
-            username={post.name}
-            userid={post.userid}
-            title={post.title}
-            date={post.date}
-            body={post.body}
-            image={post.imageobj}
-          />
-        );
-      });
+    let { posts, error, loading } = this.props;
+
+    let displayPosts = <div className="Posts" />;
+    let profileName = "Loading...";
+    if (posts && posts.length > 0 && !error && !loading) {
+      profileName = this.props.match ? posts[0].name : null;
+      displayPosts = (
+        <div className="Posts fade-in">
+          {posts.map(post => {
+            return (
+              <Post
+                key={post.id}
+                id={post.id}
+                username={post.name}
+                userid={post.userid}
+                title={post.title}
+                date={post.date}
+                body={post.body}
+                image={post.imageobj}
+              />
+            );
+          })}
+        </div>
+      );
     } else if (error && error.status === 401) {
       displayPosts = <Error error={error} />;
     }
-
     return (
       <div className="container">
-        <div className={classes.join(" ")}>{displayPosts}</div>
+        <p className="Posts__profile-name">{profileName}</p>
+        {displayPosts}
         <Link to="/posts/new">
           <button className="Posts__button">+</button>
         </Link>
@@ -53,7 +72,12 @@ class Posts extends Component {
   }
 }
 
-export default connect(
-  state => ({ ...state.postReducer, ...state.userReducer }),
-  { getPosts, getPostsByUserId }
-)(Posts);
+const mapStateToProps = state => {
+  return {
+    posts: state.postReducer.posts,
+    error: state.postReducer.error,
+    loading: state.postReducer.loading
+  };
+};
+
+export default connect(mapStateToProps, { getPosts, getPostsByUserId })(Posts);
