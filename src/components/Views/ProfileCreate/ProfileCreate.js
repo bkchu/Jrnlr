73018a0f317a714e914dcb71f2f76c29684be3a7
bkchu <brandon.k.chung@gmail.com>
2018-07-04 16:24:24 -0500

    added https to amazons3 photos

--- src/components/Views/ProfileCreate/ProfileCreate.js
+++ src/components/Views/ProfileCreate/ProfileCreate.js
@@ -1,45 +1,45 @@
-import React, { Component } from "react";
-import ReactS3Uploader from "react-s3-uploader";
-import { toast, ToastContainer } from "react-toastify";
-import { connect } from "react-redux";
-import { Redirect } from "react-router-dom";
-import Error from "../../Error/Error";
+import React, { Component } from 'react';
+import ReactS3Uploader from 'react-s3-uploader';
+import { toast, ToastContainer } from 'react-toastify';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import Error from '../../Error/Error';
 
 import {
   setUserIsNewToFalse,
   addProfile
-} from "../../../redux/ducks/userReducer";
+} from '../../../redux/ducks/userReducer';
 
-import "./ProfileCreate.css";
+import './ProfileCreate.css';
 
 class ProfileCreate extends Component {
   state = {
     photo: null,
-    fullName: "",
-    about: ""
+    fullName: '',
+    about: ''
   };
 
   onSubmitHandler = e => {
     let { fullName, about, photo } = this.state;
-    let fullNameFilled = fullName !== "";
-    let aboutFilled = about !== "";
+    let fullNameFilled = fullName !== '';
+    let aboutFilled = about !== '';
     e.preventDefault();
     if (fullNameFilled && aboutFilled) {
       // this.props.setUserIsNewToFalse();
       this.props.addProfile({
         photo: photo
           ? photo
-          : "https://www.bspmediagroup.com/event/img/logos/user_placeholder.png",
+          : 'https://www.bspmediagroup.com/event/img/logos/user_placeholder.png',
         fullName,
         about
       });
-      this.props.history.push("/");
+      this.props.history.push('/');
     } else {
       if (!fullNameFilled) {
-        toast.warn("Please fill in the Full Name field.");
+        toast.warn('Please fill in the Full Name field.');
       }
       if (!aboutFilled) {
-        toast.warn("Please fill in the About field.");
+        toast.warn('Please fill in the About field.');
       }
     }
   };
@@ -51,7 +51,7 @@ class ProfileCreate extends Component {
   onPictureUpload = s3 => {
     //check if photo state is empty, conditionally update profile: if photo state empty, then don't update photo.
     this.setState({
-      photo: `http://s3.us-east-2.amazonaws.com/react-journal-user-profile/${
+      photo: `https://s3.us-east-2.amazonaws.com/react-journal-user-profile/${
         s3.filename
       }`
     });
@@ -68,10 +68,10 @@ class ProfileCreate extends Component {
       return (
         <Error
           error={{
-            status: "A verification email has been sent to your email.",
+            status: 'A verification email has been sent to your email.',
             data: {
               error:
-                "Please log in again.\nPlease note: The verification process can take up to two minutes."
+                'Please log in again.\nPlease note: The verification process can take up to two minutes.'
             }
           }}
         />
@@ -87,7 +87,7 @@ class ProfileCreate extends Component {
             src={
               photo
                 ? photo
-                : "https://www.bspmediagroup.com/event/img/logos/user_placeholder.png"
+                : 'https://www.bspmediagroup.com/event/img/logos/user_placeholder.png'
             }
             alt="user profile"
           />
@@ -101,7 +101,7 @@ class ProfileCreate extends Component {
               onProgress={this.progress}
               onFinish={this.onPictureUpload}
               contentDisposition="auto"
-              scrubFilename={filename => filename.replace(/[^\w\d_\-.]+/gi, "")}
+              scrubFilename={filename => filename.replace(/[^\w\d_\-.]+/gi, '')}
               inputRef={cmp => (this.uploadInput = cmp)}
               server={process.env.REACT_APP_CLIENT_HOST}
               autoUpload
@@ -137,7 +137,10 @@ class ProfileCreate extends Component {
   }
 }
 
-export default connect(state => ({ ...state.userReducer }), {
-  setUserIsNewToFalse,
-  addProfile
-})(ProfileCreate);
+export default connect(
+  state => ({ ...state.userReducer }),
+  {
+    setUserIsNewToFalse,
+    addProfile
+  }
+)(ProfileCreate);
